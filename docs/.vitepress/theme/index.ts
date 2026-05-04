51dfa6183a3347f67e6a863755e7f3f2c7f37116
DawnHover <demwre@outlook.com>
2026-05-04 23:47:08 +0800

    add(widgets)：根据原页面样式，新增可嵌入markdown的组件

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,11 +2,20 @@
 import Layout from './client/Layout.vue'
 import type { Theme } from 'vitepress'
 import './client/styles/index.css'
+import Intro from './client/widgets/Intro.vue'
+import MidTitle from './client/widgets/MidTitle.vue'
+import Grid from './client/widgets/Grid.vue'
+import Card from './client/widgets/Card.vue'
+import Spacer from './client/widgets/Spacer.vue'
 
 export default {
   Layout,
   enhanceApp({ app, router, siteData }) {
-    // ...
-  }
+    // 将一些可能用的比较多的组件注册为全局组件
+    app.component('Intro', Intro)
+    app.component('MidTitle', MidTitle)
+    app.component('Grid', Grid)
+    app.component('Card', Card)
+    app.component('Space', Spacer)
+  },
 } satisfies Theme
-
